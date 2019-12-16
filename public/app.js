f9dcf21b41d41be48b0c6436e2ef1349d411be7a
Kimberlyn Creary <kimocreary@gmail.com>
2019-12-16 18:14:07 -0500

    change to app js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-$.getJSON("/articles", function(data) {
+$.getJSON("/allArticles", function(data) {
     for (var i = 0; i < data.length; i++) {
         $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
     }
